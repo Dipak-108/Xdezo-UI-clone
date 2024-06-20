506470be992ad04b0af637c723549716beb94f98
Dipak Dahal <dipakdahalconnects@gmail.com>
2024-06-20 19:01:38 +0545

    carousel

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,59 +1,22 @@
-// let slides = document.getElementsByClassName("image");
-// let currentSlide = 0;
-// slides[currentSlide].style.display = "block";
-
-// document.getElementById("next").addEventListener("click", function () {
-//   slides[currentSlide].style.display = "none";
-//   currentSlide++;
-
-//   if (currentSlide >= slides.length) {
-//     currentSlide = 0;
-//   }
-//   slides[currentSlide].style.display = "block";
-// });
-
-// document.getElementById("previous").addEventListener("click", function () {
-//   slides[currentSlide].style.display = "none";
-//   currentSlide--;
-//   if (currentSlide < 0) {
-//     currentSlide = slides.length - 1;
-//   }
-//   slides[currentSlide].style.display = "block";
-// });
-
-
-
-
 let slides = document.getElementsByClassName("image");
 let currentSlide = 0;
 slides[currentSlide].style.display = "block";
 
 document.getElementById("next").addEventListener("click", function () {
-    slides[currentSlide].style.display = "none";
-currentSlide++;
+  slides[currentSlide].style.display = "none";
+  currentSlide++;
 
-if (currentSlide .slides.length) {
-
-currentSlide = 0;
-}
-
-slides[currentSlide].style.display = "block";
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+  slides[currentSlide].style.display = "block";
 });
 
 document.getElementById("previous").addEventListener("click", function () {
-// console.log("brefore "+currentSlide);
-
-if (currentSlide >= -1) {
-slides[currentSlide + 1].style.display = "none";
-console.log(currentSlide);
-currentSlide = 3;
-slides[currentSlide].style.display = "block";
-console.log("abc" + currentSlide);
-}
-
-slides[currentSlide].style.display = "none";
-slides[currentSlide - 1].style.display = "block";
-
-currentSlide--;
-// console.log(currentSlide);
+  slides[currentSlide].style.display = "none";
+  currentSlide--;
+  if (currentSlide < 0) {
+    currentSlide = slides.length - 1;
+  }
+  slides[currentSlide].style.display = "block";
 });
