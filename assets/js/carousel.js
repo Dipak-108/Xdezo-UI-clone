15c0f3dfe5ba0918278f236672e8294ae293c20d
Dipak Dahal <dipakdahalconnects@gmail.com>
2024-06-20 18:36:58 +0545

    Carousel logic added

---
+++ assets/js/carousel.js
@@ -0,0 +1,22 @@
+let slides = document.getElementsByClassName("image");
+let currentSlide = 0;
+slides[0].style.display = "block";
+
+document.getElementById("next").addEventListener("click", function () {
+  slides[currentSlide].style.display = "none";
+  currentSlide++;
+
+  if (currentSlide >= slides.length) {
+    return (currentSlide = 0);
+  }
+  slides[currentSlide].style.display = "block";
+});
+
+document.getElementById("previous").addEventListener("click", function () {
+  slides[currentSlide].style.display = "none";
+  currentSlide--;
+  if (currentSlide <= 0) {
+    currentSlide - slides.length - 1;
+  }
+  slides[currentSlide].style.display = "block";
+});
